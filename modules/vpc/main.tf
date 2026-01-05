3276da1d82eceb25a31e70b99e986d3373b33625
Teller-dot11 <black.hole.5d.space@gmail.com>
2026-01-06 00:42:36 +0530

    Create main.tf

---
+++ modules/vpc/main.tf
@@ -0,0 +1,11 @@
+resource "google_compute_network" "vpc" {
+  name = "demo-vpc"
+  auto_create_subnetworks = false
+ }
+
+resource "google_compute_subnetwork" "subnet" {
+  name = "demo-subnet"
+  region = var.region
+  network = google_compute_network.vpc.id
+  ip_cidr_range = "10.0.0.0/24"
+  }
