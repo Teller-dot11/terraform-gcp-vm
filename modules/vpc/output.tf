9e5cf716cffdba6ed7d608deaaec8f8accefb3d3
Teller-dot11 <black.hole.5d.space@gmail.com>
2026-01-06 23:39:02 +0530

    Update output.tf

--- modules/vpc/output.tf
+++ modules/vpc/output.tf
@@ -1,7 +1,7 @@
 output "network_id" {
- value = google_compute_network.vpc.id
+ value = google_compute_network.this.id
 }
 
 output "subnet_id" {
- value = google_compute_subnetwork.subnet.id
+ value = google_compute_subnetwork.this.id
 }
