549244c587b9530dc7b67f9f905a772a578d764c
Teller-dot11 <black.hole.5d.space@gmail.com>
2026-01-06 23:38:37 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,11 +1,11 @@
-resource "google_compute_network" "vpc" {
+resource "google_compute_network" "this" {
   name = "var.vpc_network"
   auto_create_subnetworks = false
  }
 
-resource "google_compute_subnetwork" "subnet" {
+resource "google_compute_subnetwork" "this" {
   name = "${var.vpc_network}-subnet"
   region = var.region
-  network = google_compute_network.vpc.id
+  network = google_compute_network.this.id
   ip_cidr_range = "10.0.0.0/24"
 }
