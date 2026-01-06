f467eb96c918ab743c21ca4efd46ee9a6c64e9fe
Teller-dot11 <black.hole.5d.space@gmail.com>
2026-01-06 12:24:09 +0530

    Update main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,6 +1,6 @@
 resource "google_compute_network" "vpc" {
-  name = "var.vpc-name"
-  auto_create_subnetworks = false
+  name = "var.vpc_name"
+  auto_create_subnetworks = true
  }
 
 resource "google_compute_subnetwork" "subnet" {
