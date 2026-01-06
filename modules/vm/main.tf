65aaff9f4f472d0bf6c6c5f02c8432c15612bce0
Teller-dot11 <black.hole.5d.space@gmail.com>
2026-01-06 23:13:09 +0530

    Update main.tf

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -1,16 +1,16 @@
 resource "google_compute_instance" "vm" {
  name = var.vm_name
- machine_type = var.machine_type
+ machine_type = "e2.medium"
  zone = var.zone
 
  boot_disk {
   initialize_params {
-   image = "debians-cloud/debian-11"
+   image = "debians-cloud/debian-12"
    }
   }
   
  network_interface {
-  subnetwork= var.subnet_id
-  access_config {}
+  network = var.network_id
+  subnetwork = var.subnet_id
   }
 }
