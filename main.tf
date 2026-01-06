b699b8f9731b3cc28115a85a8b16f1a46232c89f
Teller-dot11 <black.hole.5d.space@gmail.com>
2026-01-06 13:17:31 +0530

    Update main.tf
    update in path

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 module "vpc" {
- source = "./module/vpc"
+ source = "./modules/vpc"
  network_name = var.network_name
  }
 
 module "vm" {
- source = "./module/vm"
+ source = "./modules/vm"
  zone = var.vm_config.zone
  machine_type = var.vm_config.machine_type
  vm_name = var.vm_config.vm_name
