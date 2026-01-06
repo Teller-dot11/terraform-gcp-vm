c1b6b754284122fd4c5fa0793d6d3e688f7af744
Teller-dot11 <black.hole.5d.space@gmail.com>
2026-01-06 12:48:06 +0530

    Update main.tf
    using tfvars data

--- main.tf
+++ main.tf
@@ -1,19 +1,14 @@
-provider "google" {
- project = var.project_id
- region = var.region
- }
-
 module "vpc" {
  source = "./module/vpc"
  network_name = var.network_name
- region = var.region
  }
 
 module "vm" {
  source = "./module/vm"
- zone = var.zone
- machine_type = var.machine_type
- vm_name = var.vm_name
+ zone = var.vm_config.zone
+ machine_type = var.vm_config.machine_type
+ vm_name = var.vm_config.vm_name
+
  network_id = module.vpc.network_id
  }
  
