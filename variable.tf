ae7533f3803e685b93b956012d05cad53ff3f249
Teller-dot11 <black.hole.5d.space@gmail.com>
2026-01-06 12:02:19 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,10 +1,16 @@
-variable "project_id" {}
+variable "project_id" {
+ type = string
+description = "GCP project ID"
+}
 variable "region" {
+  type = string
   default = "us-central1"
 }
 variable "zone" {
+  type = string
   default = "us-central1-a"
 }
 variable "vm_name" {
+  type = string
   default = "demo-vm"
 }
